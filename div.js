7066b2c8171113f8802dc0188cf2cf5115004ea5
kalyar <49766156+kalyarUserName@users.noreply.github.com>
2020-10-19 02:31:49 +0300

    Update div.js

--- div.js
+++ div.js
@@ -1,3 +1,11 @@
 function divide(a, b){
-	return a << b;
+    let sign = ((a > 0 && b < 0) || (a < 0 && b > 0)) ? -1 : 1;
+    let aa = Math.abs(a);
+    let bb = Math.abs(b);
+    let res = 0;
+    while (aa - bb >= 0) {
+        aa = aa - bb;
+        res++;
+    }
+    return sign * res;
 }
